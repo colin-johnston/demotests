446d2b0e0a5e9f282e4801012670c8beefec6318
William Josephson <wkj@solanolabs.com>
2015-08-03 22:16:42 +0000

    A sample flaky test.

---
+++ spec/flaky_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'A Flaky Test' do
+  it 'should be flaky' do
+    file_exists = File.exist?('/tmp/flaky_spec.txt')
+    FileUtils.touch('/tmp/flaky_spec.txt')
+    Kernel.sleep(10)
+    expect(file_exists).to be_truthy
+  end
+end

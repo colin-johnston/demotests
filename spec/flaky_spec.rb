6507dabcf79d68335e267b197c1c4dd155f558c0
Colin Johnston <neoterica@gmail.com>
2015-09-15 15:30:39 -0700

    change time to 60s

--- spec/flaky_spec.rb
+++ spec/flaky_spec.rb
@@ -4,7 +4,7 @@ describe 'A Flaky Test' do
   it 'should be flaky' do
     file_exists = File.exist?('/tmp/flaky_spec.txt')
     FileUtils.touch('/tmp/flaky_spec.txt')
-    Kernel.sleep(120)
+    Kernel.sleep(60)
     expect(file_exists).to be_truthy
   end
 end
